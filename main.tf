26eb0be26db13fbac6ac09aed982f050ae77a765
kondo takeshi <chaspy@users.noreply.github.com>
2019-08-18 00:31:46 +0900

    Add security group rules

--- main.tf
+++ main.tf
@@ -49,6 +49,50 @@ resource "alicloud_security_group" "allow_basic_rule" {
   vpc_id       = alicloud_vpc.vpc.id
 }
 
+resource "alicloud_security_group_rule" "allow_https" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "443/443"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.allow_basic_rule.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+resource "alicloud_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "22/22"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.allow_basic_rule.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+resource "alicloud_security_group_rule" "allow_icmp" {
+  type              = "ingress"
+  ip_protocol       = "icmp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "-1/-1"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.allow_basic_rule.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
+resource "alicloud_security_group_rule" "allow_http" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "80/80"
+  priority          = 1
+  security_group_id = "${alicloud_security_group.allow_basic_rule.id}"
+  cidr_ip           = "0.0.0.0/0"
+}
+
 output "ip" {
   value = alicloud_instance.web.public_ip
 }
