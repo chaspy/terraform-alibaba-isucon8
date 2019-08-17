32d58efd376b05a6406f010ebf53c69e6a05d0d7
kondo takeshi <chaspy@users.noreply.github.com>
2019-08-17 15:38:22 +0900

    terraform 0.12upgrade

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 data "alicloud_instance_types" "type" {
   cpu_core_count = 2
-  memory_size = 4
+  memory_size    = 4
 }
 
 data "alicloud_images" "default" {
@@ -15,26 +15,27 @@ resource "alicloud_vpc" "vpc" {
 }
 
 resource "alicloud_vswitch" "vsw" {
-  vpc_id            = "${alicloud_vpc.vpc.id}"
+  vpc_id            = alicloud_vpc.vpc.id
   cidr_block        = "172.16.0.0/21"
   availability_zone = "ap-northeast-1a"
 }
 
 # Create a web server
 resource "alicloud_instance" "web" {
-  image_id          = "${data.alicloud_images.default.images.0.id}"
-  internet_charge_type  = "PayByBandwidth"
+  image_id             = data.alicloud_images.default.images[0].id
+  internet_charge_type = "PayByBandwidth"
 
-  instance_type        = "${data.alicloud_instance_types.type.instance_types.0.id}"
+  instance_type        = data.alicloud_instance_types.type.instance_types[0].id
   system_disk_category = "cloud_efficiency"
-  security_groups      = ["${alicloud_security_group.default.id}"]
+  security_groups      = [alicloud_security_group.default.id]
   instance_name        = "web"
-  vswitch_id = "${alicloud_vswitch.vsw.id}"
+  vswitch_id           = alicloud_vswitch.vsw.id
 }
 
 # Create security group
 resource "alicloud_security_group" "default" {
   name        = "default"
   description = "default"
-  vpc_id = "${alicloud_vpc.vpc.id}"
+  vpc_id      = alicloud_vpc.vpc.id
 }
+
--- provider.tf
+++ provider.tf
@@ -1,1 +1,3 @@
-provider "alicloud" {}
+provider "alicloud" {
+}
+
